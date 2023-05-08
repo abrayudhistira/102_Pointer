975a02c44f6f1cafce92e026ee4a86f3e92afafa
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-08 10:52:53 +0700

    membuat class modifier

--- 102_Pointer/102_Pointer.cpp
+++ 102_Pointer/102_Pointer.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class mahasiswa{						//membuat class mahasiswa
-	public:
+	public:								//membuat class modifier
 	int nim;
 	void showNim() {
 		cout << "No Induk = " << nim << endl;
